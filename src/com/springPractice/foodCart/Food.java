ac8e227b193554d919d2f325206899e3b44e75a3
alexYamaoka <ryamaoka1@gmail.com>
2020-09-10 12:36:06 -0700

    created constructor and getter and setter for Food class

--- src/com/springPractice/foodCart/Food.java
+++ src/com/springPractice/foodCart/Food.java
@@ -1,5 +1,41 @@
 package com.springPractice.foodCart;
 
 public class Food {
+	private int id;
+	private String item;
+	private double price;
+	
+	public Food(int id, String item, double price) {
+		super();
+		this.id = id;
+		this.item = item;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 
 }
